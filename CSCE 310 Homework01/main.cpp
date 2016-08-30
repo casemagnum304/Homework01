3049c4b56a0fa2f700bb5f6bc7bdd9f9448f1d87
casemagnum304 <codymumm@codys-macbook-pro.local>
2016-08-29 20:29:51 -0500

    Initial Commit

---
+++ CSCE 310 Homework01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSCE 310 Homework01
+//
+//  Created by Cody Mumm on 8/29/16.
+//  Copyright © 2016 Cody Mumm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
